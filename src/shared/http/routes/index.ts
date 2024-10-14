df5657f55f7960a59949a6b6b58eb86d354d783e
Joao Vitor Augusto de Lima <joao.vitor@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2024-10-14 16:52:46 -0300

    add controller and routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router, Request, Response } from "express";
+import productsRouter from "@modules/products/routes/products.routes";
 
 const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
-    return res.json({ message: "Hello Dev!" });
-});
+routes.use("/products", productsRouter);
 
 export default routes;
